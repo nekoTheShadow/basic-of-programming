fd865520d33529d76d282638d61a0e1c02de9f2a
hajime nakamura <h.nakamura0903@gmail.com>
2020-10-03 22:25:15 +0900

    第4章 関数の定義

---
+++ BasicOfProgramming/Chapter04.fs
@@ -0,0 +1,22 @@
+module Chapter04
+
+// 問題4.1
+let baitoKyuyo year hour = (850+year*100)*hour
+
+// 問題4.2
+let jikoshokai name = "わたしの名前は" + name + "です"
+
+// 問題4.3
+let hyoujunTaiju meter = meter**2.0 * 22.0
+
+// 問題4.4
+let bmi meter kg = kg / (meter**2.0)
+
+// 問題4.5
+let tsuruNoAshi tsuru = tsuru*2
+
+// 問題4.6
+let tsusuKameNoAshi tsuru kame = tsuru*2+kame*4
+
+// 問題4.7
+let tsurukame hiki ashi = (4*hiki - ashi)/2
---
+++ BasicOfProgrammingTest/Chapter04Test.fs
@@ -0,0 +1,48 @@
+module Tests
+
+open Xunit
+open Chapter04
+
+[<Fact>]
+let ``問題4.1 baitoKyuyoはその月の給与を返す`` () =
+    let actual = baitoKyuyo 1 30
+    Assert.Equal(28500, actual)
+
+[<Fact>]
+let ``問題4.2 jikoshokaiは与えられた名前を使った自己紹介をする`` () =
+    let actual = jikoshokai "田中"
+    Assert.Equal("わたしの名前は田中です", actual)
+
+[<Fact>]
+let ``問題4.3 hyojunTaijuは与えられた身長(m)をもとに標準体重(kg)を求める`` () =
+    let actual = hyoujunTaiju 1.6
+    Assert.Equal(56.32, actual, 2)
+   
+[<Fact>]
+let ``問題4.4 bmiはBMI指数を求める`` () =
+    let actual = bmi 1.6 60.0
+    Assert.Equal(23.44, actual, 2)
+
+[<Theory>]
+[<InlineData(1, 2)>]
+[<InlineData(2, 4)>]
+[<InlineData(3, 6)>]
+let ``問題4.6 tsuruNoAshiは鶴の数から足の本数の合計を計算する`` (tsuru, expected) =
+    let actual = tsuruNoAshi tsuru
+    Assert.Equal(expected, actual)
+   
+[<Theory>]
+[<InlineData(1, 1, 6)>]
+[<InlineData(2, 2, 12)>]
+[<InlineData(3, 3, 18)>]
+let ``問題4.7 tsusuKameNoAshiは鶴と亀の数から足の本数の合計を計算する`` (tsuru, kame, expected) =
+    let actual = tsusuKameNoAshi tsuru kame
+    Assert.Equal(expected, actual)
+
+[<Theory>]
+[<InlineData(50, 156, 22)>]
+[<InlineData(10, 32, 4)>]
+[<InlineData(8, 26, 3)>]
+let ``問題4.7 tsusuKameは鶴と亀の頭数と足の本数の合計から鶴の頭数を計算する`` (hiki, ashi, expected) =
+    let actual = tsurukame hiki ashi
+    Assert.Equal(expected, actual)
