eec7485105f4d090f0e0e040b36b24f95f32d861
hajime nakamura <h.nakamura0903@gmail.com>
2020-10-03 23:19:00 +0900

    問題5.3

--- BasicOfProgramming/Chapter05.fs
+++ BasicOfProgramming/Chapter05.fs
@@ -1,3 +1,33 @@
 module Chapter05
 
+open System
+
 let jikan hour = if hour <= 12 then "午前" else "午後"
+
+let seiza month day = 
+    let between m1 d1 m2 d2 = 
+        let date = DateTime(2020, month, day)
+        let date1 = DateTime(2020, m1, d1)
+        let date2 = DateTime(2020, m2, d2)
+        date1 <= date && date <= date2
+
+    let ranges = [
+        ( 3, 21,  4, 19, "おひつじ座");
+        ( 4, 20,  5, 20, "おうし座");
+        ( 5, 21,  6, 21, "ふたご座");
+        ( 6, 23,  7, 22, "かに座");
+        ( 7, 23,  8, 22, "しし座");
+        ( 8, 23,  9, 22, "おとめ座");
+        ( 9, 23, 10, 23, "てんびん座");
+        (10, 24, 11, 22, "さそり座"); 
+        (11, 23, 12, 21, "いて座"); 
+        (12, 22,  1, 19, "やぎ座");
+        ( 1, 20,  2, 18, "みずがめ座");
+        ( 2, 19,  3, 20, "うお座");
+    ]
+
+    let _, _, _, _, sign = ranges |> List.find(fun (m1, d1, m2, d2, _) -> between m1 d1 m2 d2)
+    sign
+
+let date month day =
+    DateTime(2020, month, day)
--- BasicOfProgrammingTest/Chapter05Test.fs
+++ BasicOfProgrammingTest/Chapter05Test.fs
@@ -9,3 +9,12 @@ open Chapter05
 let ``問題5.2 jikanは午後か午前かを判定する`` (hour, expected) = 
     let actual = jikan hour
     Assert.Equal(expected, actual)
+
+[<Theory>]
+[<InlineData(6, 11, "ふたご座")>]
+[<InlineData(6, 30, "かに座")>]
+[<InlineData(9, 17, "おとめ座")>]
+[<InlineData(10, 07, "てんびん座")>]
+let ``問題5.3 seizaは与えられた日付の12星座を導出する`` (month, day, expected) = 
+    let actual = seiza month day
+    Assert.Equal(expected, actual)
