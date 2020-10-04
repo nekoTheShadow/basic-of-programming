203610d6ef16ff8c611dcf1195be4db4354078b4
hajime nakamura <h.nakamura0903@gmail.com>
2020-10-04 11:22:32 +0900

    問題7.2

--- BasicOfProgramming/Chapter07.fs
+++ BasicOfProgramming/Chapter07.fs
@@ -1,6 +1,11 @@
 module Chapter07
 
+open System
+
 let goukeiToHeikin japanese math english science society = 
     let sum = japanese+math+english+science+society
     let avg = sum/5
     (sum, avg)
+
+let seiseki (name, grade) = 
+   name + "さんの評価は" + grade + "です"
--- BasicOfProgrammingTest/Chapter07Test.fs
+++ BasicOfProgrammingTest/Chapter07Test.fs
@@ -11,3 +11,11 @@ let ``問題7.1 goukeiNoHeikinは5強化の合計点と平均点を計算する`
     let (actualSum, actualAvg) = goukeiToHeikin japanese math english science society
     Assert.Equal(sum, actualSum)
     Assert.Equal(avg, actualAvg)
+
+[<Theory>]
+[<InlineData("田中", "A", "田中さんの評価はAです")>]
+[<InlineData("山田", "B", "山田さんの評価はBです")>]
+[<InlineData("木村", "C", "木村さんの評価はCです")>]
+let ``問題7.2 seisekiは「XXさんの評価はXです」という文字列を作成する`` (name, grade, expected) =
+    let actual = seiseki (name, grade)
+    Assert.Equal(expected, actual)
