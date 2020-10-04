22d6dcde1d420e0ca156c50214173ddc5dfc4797
hajime nakamura <h.nakamura0903@gmail.com>
2020-10-04 20:14:23 +0900

    問題10.3

--- BasicOfProgramming/Chapter10.fs
+++ BasicOfProgramming/Chapter10.fs
@@ -13,3 +13,23 @@ let rec insSort lst =
     match lst with
     | [] -> []
     | x::xs -> insert (insSort xs) x
+
+type Gakusei = {
+    Namae: string;
+    Tensu: int;
+    Seiseki: string;
+}
+
+let rec gakuseiInsert students student = 
+    match students with
+    | [] -> [student]
+    | x::xs -> 
+        if student.Tensu <= x.Tensu then 
+            student :: x :: xs
+        else
+            x :: (gakuseiInsert xs student)
+
+let rec gakuseiSort students = 
+    match students with 
+    | [] -> []
+    | x::xs ->  gakuseiInsert (gakuseiSort xs) x
--- BasicOfProgrammingTest/Chapter10Test.fs
+++ BasicOfProgrammingTest/Chapter10Test.fs
@@ -11,3 +11,21 @@ let ``問題10.1 insertは昇順となる位置にnを挿入する`` () =
 [<Fact>]
 let ``問題10.2 inSortは整数のリストを昇順に並び替える`` () =
     isEqual [1; 3; 4; 5; 7; 8] (insSort [5; 3; 8; 1; 7; 4])
+
+[<Fact>]
+let ``問題10.3 gakuseiSortは学生を成績の昇順でソートする。`` () =
+    let expected = [
+        {Namae="A"; Tensu=10; Seiseki="A"};
+        {Namae="B"; Tensu=20; Seiseki="B"};
+        {Namae="C"; Tensu=30; Seiseki="C"};
+        {Namae="D"; Tensu=40; Seiseki="D"};
+        {Namae="E"; Tensu=50; Seiseki="E"};
+    ]
+    let students = [
+        {Namae="D"; Tensu=40; Seiseki="D"};
+        {Namae="A"; Tensu=10; Seiseki="A"};
+        {Namae="E"; Tensu=50; Seiseki="E"};
+        {Namae="C"; Tensu=30; Seiseki="C"};
+        {Namae="B"; Tensu=20; Seiseki="B"};
+    ]
+    isEqual expected (gakuseiSort students)
