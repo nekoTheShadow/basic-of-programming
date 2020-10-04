a4f11cc858909c6b7e1bf3301360d73cab323d49
hajime nakamura <h.nakamura0903@gmail.com>
2020-10-04 14:20:12 +0900

    問題9.1 問題9.2 問題9.3

---
+++ BasicOfProgramming/Chapter09.fs
@@ -0,0 +1,9 @@
+module Chapter09
+
+open Chapter08
+
+let seasons = "春" :: "夏" :: "秋" :: "冬" ::[]
+let persons = {Name="田中"; Height=1.6; Weight=60.0; Date="2020/01/01"; BloodType="A"} ::
+              {Name="山田"; Height=1.7; Weight=50.0; Date="2020/02/01"; BloodType="B"} ::
+              {Name="中田"; Height=1.8; Weight=40.0; Date="2020/03/01"; BloodType="O"} :: []
+let weekdays = ["日曜日"; "月曜日"; "火曜日"; "水曜日"; "木曜日"; "金曜日"; "土曜日"]
