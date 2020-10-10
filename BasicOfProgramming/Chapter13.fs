ee1a12150f20dbbe6828c2ba45c66a46166ebc47
hajime nakamura <h.nakamura0903@gmail.com>
2020-10-10 21:33:06 +0900

    問題13.3

--- BasicOfProgramming/Chapter13.fs
+++ BasicOfProgramming/Chapter13.fs
@@ -13,3 +13,10 @@ let rec countKetsueki persons bloodType =
 
 let personNamae persons = 
     List.map (fun p -> p.Name) persons
+
+// 問題13.3
+let f1 x = x
+let f2 x y = x
+let f3 x y = y
+let f4 x f = (f x)
+let f5 f g x = g (f x)
