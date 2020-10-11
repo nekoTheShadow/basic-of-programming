cc7b7e7264fb4140a82aac1174b98c6f3be88812
hajime nakamura <h.nakamura0903@gmail.com>
2020-10-11 18:08:07 +0900

    問題14.14

--- BasicOfProgramming/Chapter14.fs
+++ BasicOfProgramming/Chapter14.fs
@@ -12,7 +12,7 @@ let even lst = List.filter (fun v -> v%2=0) lst
 
 let countA gakuseiList = List.length (List.filter (fun gakusei -> gakusei.Seiseki = "A") gakuseiList)
 
-let concat lst = List.fold ((+)) "" lst
+let concat lst = List.fold (+) "" lst
 
 let gakuseiSum gakuseiList = List.sumBy (fun gakusei -> gakusei.Tensuu) gakuseiList
 
