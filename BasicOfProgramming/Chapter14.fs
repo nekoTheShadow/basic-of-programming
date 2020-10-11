c3fbcee144ba844afe5a8ab55ab817625692c08c
hajime nakamura <h.nakamura0903@gmail.com>
2020-10-11 17:41:50 +0900

    問題14.2

--- BasicOfProgramming/Chapter14.fs
+++ BasicOfProgramming/Chapter14.fs
@@ -1,3 +1,11 @@
 module Chapter14
 
+type Gakusei = {
+    Namae: string;
+    Tensuu: int;
+    Seiseki: string;
+}
+
 let even lst = List.filter (fun v -> v%2=0) lst
+
+let countA gakuseiList = List.length (List.filter (fun gakusei -> gakusei.Seiseki = "A") gakuseiList)
--- BasicOfProgrammingTest/Chapter14Test.fs
+++ BasicOfProgrammingTest/Chapter14Test.fs
@@ -5,5 +5,16 @@ open Chapter14
 open TestUtil
 
 [<Fact>]
-let ``問題14.5 evenはリストの偶数の要素のみを集めたリストを作成する`` () =
+let ``問題14.1 evenはリストの偶数の要素のみを集めたリストを作成する`` () =
     isEqual [2; 6; 4] (even [2; 1; 6; 4; 7])
+
+[<Fact>]
+let ``問題14.2 countAは成績がAの学生の数を数える`` () =
+    let gakuesiList = [
+        {Namae="1"; Tensuu=1; Seiseki="A"};
+        {Namae="2"; Tensuu=2; Seiseki="B"};
+        {Namae="3"; Tensuu=3; Seiseki="C"};
+        {Namae="4"; Tensuu=4; Seiseki="A"};
+        {Namae="5"; Tensuu=5; Seiseki="B"};
+    ]
+    Assert.Equal(2, (countA gakuesiList))
