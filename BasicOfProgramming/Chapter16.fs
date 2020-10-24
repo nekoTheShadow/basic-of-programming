a68b4bf5233033b37e9d170182d31ceeef7ccb67
hajime nakamura <h.nakamura0903@gmail.com>
2020-10-24 18:04:10 +0900

    問題16.2

--- BasicOfProgramming/Chapter16.fs
+++ BasicOfProgramming/Chapter16.fs
@@ -6,3 +6,9 @@ let sumList lst =
         | [] -> []
         | d::ds -> (sum + d)::(f ds (sum + d))
     in f lst 0
+
+let rec foldLeft f init lst =
+    let rec g acc lst = match lst with
+        | [] -> acc
+        | x::xs -> g (f acc x) xs
+    in g init lst
--- BasicOfProgrammingTest/Chapter16Test.fs
+++ BasicOfProgrammingTest/Chapter16Test.fs
@@ -6,6 +6,11 @@ open TestUtil
 
 [<Fact>]
 let ``問題16.1 sumListは整数のリストからそれまでの数の合計からなるリストを作成する``() =
-    isEqual [3; 5; 6; 10] (sumList [3; 2; 1])
+    isEqual [3; 5; 6; 10] (sumList [3; 2; 1; 4])
     isEqual [] (sumList [])
 
+[<Fact>]
+let ``問題16.2 foldLeftは畳み込み関数を提供する``() =
+    Assert.Equal(0, (foldLeft (-) 0 []))
+    Assert.Equal(2, (foldLeft (-) 10 [4; 1; 3]))
+    isEqual [3; 2; 1] (foldLeft (fun lst a -> a::lst) [] [1; 2; 3]) 
