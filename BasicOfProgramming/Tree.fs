dbef1a06f0cb1b15beb21e5e7e7cec6d22bfd0cd
hajime nakamura <h.nakamura0903@gmail.com>
2020-10-31 22:59:14 +0900

    問題17.6

--- BasicOfProgramming/Tree.fs
+++ BasicOfProgramming/Tree.fs
@@ -8,3 +8,8 @@ let rec treeDouble tree = match tree with
     | Empty -> Empty
     | Leaf(n) -> Leaf(n*2)
     | Node(tree1, n, tree2) -> Node ((treeDouble tree1), n*2, (treeDouble tree2))
+
+let rec treeMap tree f = match tree with
+    | Empty -> Empty
+    | Leaf(n) -> Leaf(f(n))
+    | Node(tree1, n, tree2) -> Node ((treeMap tree1 f), (f n), (treeMap tree2 f))
--- BasicOfProgrammingTest/TreeTest.fs
+++ BasicOfProgrammingTest/TreeTest.fs
@@ -19,3 +19,21 @@ let ``問題17.5 treeDoubleは格納されている値をすべて2倍にする`
     Assert.Equal(treeB, (treeDouble tree2))
     Assert.Equal(treeC, (treeDouble tree3))
     Assert.Equal(treeD, (treeDouble tree4))
+
+
+[<Fact>]
+let ``問題17.6 treeMapは格納されている値にすべてコールバックを適用する。`` () =
+    let tree1 = Empty
+    let tree2 = Leaf(3)
+    let tree3 = Node (tree1, 4, tree2)
+    let tree4 = Node (tree2, 5, tree3)
+
+    let treeA = Empty
+    let treeB = Leaf(-3)
+    let treeC = Node (treeA, -4, treeB)
+    let treeD = Node (treeB, -5, treeC)
+
+    Assert.Equal(treeA, (treeMap tree1 ( ~- )))
+    Assert.Equal(treeB, (treeMap tree2 ( ~- )))
+    Assert.Equal(treeC, (treeMap tree3 ( ~- )))
+    Assert.Equal(treeD, (treeMap tree4 ( ~- )))
