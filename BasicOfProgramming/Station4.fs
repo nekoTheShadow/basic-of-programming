e04f6ad993a46731fa0f22b03328a37b47e8a478
hajime nakamura <h.nakamura0903@gmail.com>
2020-11-02 23:33:43 +0900

    問題20.5 20.6

---
+++ BasicOfProgramming/Station4.fs
@@ -0,0 +1,39 @@
+module Station4
+
+open Station
+open RedBlackTree
+
+let insertEkikan ekikanTree ekikan =
+    let insert ekikanTree kiten shuten kyori =
+        try 
+            let xs = search ekikanTree kiten in
+            insert ekikanTree kiten ((shuten, kyori)::xs)
+        with NotFoundException ->
+            insert ekikanTree kiten [(shuten, kyori)] in
+    insert (insert ekikanTree ekikan.Kiten ekikan.Shuten ekikan.Kyori) ekikan.Shuten ekikan.Kiten ekikan.Kyori
+
+let rec getEkikanKyori kanji1 kanji2 ekikanTree =
+    assoc kanji1 (search ekikanTree kanji2)
+
+let koushin p v ekikanTree =
+    List.map (fun q ->
+        let kyori = getEkikanKyori p.Namae q.Namae ekikanTree in
+        if kyori = infinity || p.SaitanKyori + kyori >= q.SaitanKyori then
+            q
+        else
+            {Namae=q.Namae; SaitanKyori=p.SaitanKyori + kyori; TemaeList=q.Namae::p.TemaeList}
+    ) v
+
+let rec dijkstraMain ekiList ekikanTree = 
+    match ekiList with
+    | [] -> []
+    | e::es ->
+        let (f, fs) = saitanWoBunri (e::es) in
+        let gs = koushin f fs ekikanTree in
+        f :: dijkstraMain gs ekikanTree
+
+let dijkstra startRomaji endRomaji = 
+    let startKanji = romajiToKanji startRomaji (seiretsu globalEkimeiList) in
+    let endKanji = romajiToKanji endRomaji (seiretsu globalEkimeiList) in
+    let ekiList = dijkstraMain (makeInitialEkiList globalEkimeiList startKanji) (List.fold (insertEkikan) Empty globalEkikanList) in
+    List.find (fun eki -> eki.Namae = endKanji) ekiList
---
+++ BasicOfProgrammingTest/Station4Test.fs
@@ -0,0 +1,12 @@
+module Station4Test
+
+open Xunit
+open Station
+open Station4
+
+[<Fact>]
+let ``問題20.5 dijkstraは始点(ローマ字)から終点(ローマ字)までの最短距離を求める`` () =
+    let expected1 = {Namae = "護国寺"; SaitanKyori = 9.8; TemaeList = ["護国寺"; "江戸川橋"; "飯田橋"; "市ヶ谷"; "麹町"; "永田町"; "青山一丁目"; "表参道"; "渋谷"]} 
+    let expected2 = {Namae = "目黒"; SaitanKyori = 12.7000000000000028; TemaeList = ["目黒"; "白金台"; "白金高輪"; "麻布十番"; "六本木一丁目"; "溜池山王"; "永田町"; "麹町"; "市ヶ谷"; "飯田橋"; "後楽園"; "茗荷谷"]} 
+    Assert.Equal(expected1, (dijkstra "shibuya" "gokokuji"))
+    Assert.Equal(expected2, (dijkstra "myogadani" "meguro"))
