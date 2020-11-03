5c7b0e8745eae100d6356190a9cc52cc7a4fa66a
hajime nakamura <h.nakamura0903@gmail.com>
2020-11-03 11:49:18 +0900

    問題22.2

--- BasicOfProgramming/Chapter22.fs
+++ BasicOfProgramming/Chapter22.fs
@@ -4,3 +4,12 @@ let count = ref -1
 let getnsym s =
     count := !count + 1
     sprintf "%s%d" s !count
+
+let fibArray arr = 
+    let rec f n x1 x2 = 
+        if n = Array.length arr then
+            arr
+        else
+            arr.[n] <- x1
+            f (n + 1) x2 (x1 + x2)
+    in f 0 0 1
--- BasicOfProgrammingTest/Chapter22Test.fs
+++ BasicOfProgrammingTest/Chapter22Test.fs
@@ -8,4 +8,7 @@ let ``問題22.1 gensymは文字列に順番にsuffixをつける``() =
     Assert.Equal("a0", getnsym "a")
     Assert.Equal("b1", getnsym "b")
     Assert.Equal("c2", getnsym "c")
-    
+
+[<Fact>]
+let ``問題22.2 fibArrayは配列にフィボナッチ数列を代入する``() =
+    Assert.True(Array.forall2 (=) [|0; 1; 1; 2; 3; 5; 8; 13; 21; 34;|] (fibArray [|0; 0; 0; 0; 0; 0; 0; 0; 0; 0;|]))
